88760926c50e609e66e25e1d8bd1e211c98972bf
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-12-02 12:35:29 +0300

    change header structure

--- src/components/BaseLayout/Header/index.tsx
+++ src/components/BaseLayout/Header/index.tsx
@@ -2,21 +2,26 @@ import {Button} from "../../ui/Button";
 import logo from './../../../assets/logos/Logo_Short.png'
 import {Input} from "../../ui/Input";
 import userPhoto from './../../../assets/user_photo.png'
+import styles from './index.module.sass'
 
 export const Header = (props: {}) => {
     return (
-        <>
-            <Button onClick={() => {}} title='Коллекция' />
-            <Button onClick={() => {}} title='Добавить' />
-            <img src={logo} className='logoShort' alt='логотип'/>
-            <Input onChange={() => {}} value='Поиск' type='text'/>
-            <UserThumbnail image={userPhoto} />
-        </>
+        <div className={styles.header}>
+            <div className={styles.elementsBunch}>
+                <Button onClick={() => {}} title='Коллекция' />
+                <Button onClick={() => {}} title='Добавить' />
+            </div>
+            <img src={logo} className={styles.logoShort} alt='логотип'/>
+            <div className={styles.elementsBunch}>
+                <Input onChange={() => {}} value='Поиск' type='text'/>
+                <UserThumbnail image={userPhoto} />
+            </div>
+        </div>
     )
 }
 
 const UserThumbnail = (props: {image: string}) => {
     return (
-        <img className='userThumbnail' src={props.image} />
+        <img className='userThumbnail' src={props.image} alt='Фотография пользователя'/>
     )
 }
